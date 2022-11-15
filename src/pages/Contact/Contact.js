1038c47e78b39f06f0c8938381625dd8947c9fe4
Sneha Shaw <snehashaw122@gmail.com>
2022-11-15 20:09:24 +0530

    `feat`: `contact`

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,12 +1,59 @@
 import React from 'react'
-
-// import GitHubIcon from '@mui/icons-material/GitHub';
-// import FacebookIcon from '@mui/icons-material/Facebook';
-// import LinkedInIcon from '@mui/icons-material/LinkedIn';
-// import MailIcon from '@mui/icons-material/Mail';
+import { useStyles } from './styles'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MailIcon from '@mui/icons-material/Mail';
+import { Link } from 'react-router-dom';
 const Contact = () => {
+  const classes = useStyles()
   return (
-    <div>Contact</div>
+    <div className={classes.Maincontainer}>
+      <h1 className={classes.title}>CONTACT <span>ME</span></h1>
+      {/* mail me form */}
+      <div className={classes.container}>
+        <div className={classes.left}>
+        <div className={classes.wrapper}>
+          <h2>
+            Let's <span>create</span> Something Together
+          </h2>
+             {/* <p>
+              You can also find me on these platforms
+            </p> */}
+            <div className={classes.social}>
+              <Link to="">
+                <GitHubIcon className={classes.icon} />
+              </Link>
+              <Link to="">
+                <FacebookIcon className={classes.icon} />
+              </Link>
+              <Link to="">
+                <LinkedInIcon className={classes.icon} />
+              </Link>
+              <Link to="">
+                <MailIcon className={classes.icon} />
+              </Link>
+            </div> 
+          </div>
+         
+        </div>
+        <div className={classes.right}>
+        <div className={classes.formWrapper}>
+            <h2>
+              Don't hesitate to drop me a Hi !
+            </h2>
+            <form className={classes.form}>
+              <input type="text" placeholder="Name" className={classes.input} />
+              <input type="email" placeholder="Email" className={classes.input} />
+              <textarea placeholder="Message" className={classes.input} />
+              <button className={classes.button}>Send</button>
+            </form>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   )
 }
 
--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -1,4 +1,167 @@
 import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles((theme) => ({
+    Maincontainer: {
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#f5f5f5",
+    },
+    title: {
+        fontSize: "8rem",
+        letterSpacing: "2rem",
+        color: "#333",
+        textAlign: "center",
+        "& span": {
+            color: "#f50057",
+        },
+    },
+    container: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        [theme.breakpoints.down("md")]: {
+            flexDirection: "column",
+        },
+    },
+    left: {
+        width: "50%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        [theme.breakpoints.down("md")]: {
+            width: "100%",
+            height: "50%",
+        },
+    },
+    right: {
+        width: "50%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        [theme.breakpoints.down("md")]: {
+            width: "100%",
+            height: "50%",
+        },
+    },
+    formWrapper: {
+        width: "80%",
+        height: "60%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        "& h2": {
+            fontSize: "3rem",
+            color: "#333",
+            textAlign: "center",
+            marginBottom: "2rem",
+        },
+        [theme.breakpoints.down("md")]: {
+            width: "100%",
+            height: "100%",
+        },
+    },
+    form: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        "& input": {
+            width: "100%",
+            height: "5rem",
+            margin: "1rem 0",
+            padding: "0 2rem",
+            fontSize: "1.2rem",
+            border: "none",
+            outline: "none",
+            backgroundColor: "rgba(1,1,1,0.1)",
+            borderRadius: "2rem",
+        },
+        "& textarea": {
+            width: "100%",
+            height: "10rem",
+            margin: "1rem 0",
+            padding: "2rem",
+            fontSize: "1.2rem",
+            border: "none",
+            outline: "none",
+            backgroundColor: "rgba(1,1,1,0.1)",
+            borderRadius: "2rem",
+            resize: "none"
+        },
+        "& button": {
+            width: "40%",
+            height: "5rem",
+            margin: "1rem 0",
+            padding: "0 2rem",
+            fontSize: "2rem",
+            border: "none",
+            outline: "none",
+            backgroundColor: "#f50057",
+            color: "#fff",
+            borderRadius: "2rem",
+            cursor: "pointer",
+        }
+    },
+    wrapper: {
+        width: "80%",
+        height: "60%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        "& h2": {
+            width: "40%",
+            fontSize: "5rem",
+            color: "#333",
+            textAlign: "left",
+            marginBottom: "2rem",
+            lineHeight: "1.5",
+            "& span": {
+                color: "#f50057",
+            },
+        },
+        [theme.breakpoints.down("md")]: {
+            width: "100%",
+            height: "100%",
+        },
+    },
+    social:{
+        width: "80%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        "& a": {
+            width: "5rem",
+            height: "5rem",
+            margin: "0 1rem",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: "2rem",
+            color: "#fff",
+            borderRadius: "50%",
+            backgroundColor: "#333",
+            cursor: "pointer",
+            transition: "all 0.5s ease",
+            "&:hover": {
+                backgroundColor: "#f50057",
+            }
+        }
+    },
+    icon:{
+        fontSize: "2.5rem !important",
+    }
 }));
