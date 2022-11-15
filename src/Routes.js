0d856ec3307848a717ba6bde65697f803998f28d
Sneha Shaw <snehashaw122@gmail.com>
2022-11-15 12:15:12 +0530

    `new react app`

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+const Router = () => {
+
+    return (
+        // <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Routes>
+                </Routes>
+            </BrowserRouter>
+        // </ThemeProvider>
+
+    )
+}
+
+export default Router
