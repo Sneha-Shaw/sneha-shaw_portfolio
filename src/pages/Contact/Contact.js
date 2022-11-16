e3e4372ebde165967e1cce73700b47eefce4412e
Sneha Shaw <snehashaw122@gmail.com>
2022-11-16 13:33:55 +0530

    `email setup`

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useStyles } from './styles'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import MailIcon from '@mui/icons-material/Mail';
+import Swal from 'sweetalert2';
 
 const Contact = () => {
   const classes = useStyles()
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const templateId = 'template_t7dw9lw'
+    // console.log(message);
+    sendFeedback(templateId, {
+      message: message,
+      from_name: name,
+      reply_to: email
+    })
+    setName('')
+    setEmail('')
+    setMessage('')
+  }
+  const sendFeedback = (templateId, variables) => {
+    window.emailjs.send(
+      'service_d435bqq', templateId,
+      variables
+    ).then(
+      res => {
+        // console.log('Email successfully sent!')
+        Swal.fire({
+          title: 'Success!',
+          text: 'Your message has been sent',
+          icon: 'success',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    ).catch(
+      err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err)
+    )
+  }
   return (
     <div className={classes.Maincontainer}>
       <h1 className={classes.title}>Contact <span>Me</span></h1>
@@ -36,7 +72,7 @@ const Contact = () => {
               <p>
                 snehashaw122@gmail.com
               </p>
-              </div>
+            </div>
 
           </div>
 
@@ -47,10 +83,27 @@ const Contact = () => {
               Don't hesitate to drop me a Hi !
             </h2>
             <form className={classes.form}>
-              <input type="text" placeholder="Name" className={classes.input} />
-              <input type="email" placeholder="Email" className={classes.input} />
-              <textarea placeholder="Message" className={classes.input} />
-              <button className={classes.button}>Send</button>
+              <input type="text"
+                placeholder="Name"
+                className={classes.input}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input type="email"
+                placeholder="Email"
+                className={classes.input}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <textarea
+                placeholder="Message"
+                className={classes.input}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <button className={classes.button}
+                onClick={handleSubmit}
+              >Send</button>
             </form>
           </div>
         </div>
